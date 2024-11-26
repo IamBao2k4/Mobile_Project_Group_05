523cc1c9f0426b4ef902f1cecac2705b55fad8a8
lebao2612 <22120022@student.hcmus.edu.vn>
2024-11-26 22:53:09 +0700

    editImage

--- sdk/build/intermediates/cxx/Debug/2246z4h2/logs/x86/configure_command.bat
+++ sdk/build/intermediates/cxx/Debug/2246z4h2/logs/x86/configure_command.bat
@@ -1,19 +1,19 @@
 @echo off
-"C:\\Users\\phanm\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
-  "-HC:\\Users\\phanm\\OneDrive\\Desktop\\University\\Third_year\\First_semester\\MobileDevelop\\project\\Mobile_Project_Group_05\\sdk\\libcxx_helper" ^
+"D:\\Android\\SDK\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HD:\\HCMUS Slide\\Nam 3\\Phat trien phan mem di dong\\Mobile_Project_Group_05\\sdk\\libcxx_helper" ^
   "-DCMAKE_SYSTEM_NAME=Android" ^
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=21" ^
   "-DANDROID_PLATFORM=android-21" ^
   "-DANDROID_ABI=x86" ^
   "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
-  "-DANDROID_NDK=C:\\Users\\phanm\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
-  "-DCMAKE_ANDROID_NDK=C:\\Users\\phanm\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
-  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\phanm\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125\\build\\cmake\\android.toolchain.cmake" ^
-  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\phanm\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\phanm\\OneDrive\\Desktop\\University\\Third_year\\First_semester\\MobileDevelop\\project\\Mobile_Project_Group_05\\sdk\\build\\intermediates\\cxx\\Debug\\2246z4h2\\obj\\x86" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\phanm\\OneDrive\\Desktop\\University\\Third_year\\First_semester\\MobileDevelop\\project\\Mobile_Project_Group_05\\sdk\\build\\intermediates\\cxx\\Debug\\2246z4h2\\obj\\x86" ^
+  "-DANDROID_NDK=D:\\Android\\SDK\\ndk\\26.1.10909125" ^
+  "-DCMAKE_ANDROID_NDK=D:\\Android\\SDK\\ndk\\26.1.10909125" ^
+  "-DCMAKE_TOOLCHAIN_FILE=D:\\Android\\SDK\\ndk\\26.1.10909125\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=D:\\Android\\SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\HCMUS Slide\\Nam 3\\Phat trien phan mem di dong\\Mobile_Project_Group_05\\sdk\\build\\intermediates\\cxx\\Debug\\2246z4h2\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\HCMUS Slide\\Nam 3\\Phat trien phan mem di dong\\Mobile_Project_Group_05\\sdk\\build\\intermediates\\cxx\\Debug\\2246z4h2\\obj\\x86" ^
   "-DCMAKE_BUILD_TYPE=Debug" ^
-  "-BC:\\Users\\phanm\\OneDrive\\Desktop\\University\\Third_year\\First_semester\\MobileDevelop\\project\\Mobile_Project_Group_05\\sdk\\.cxx\\Debug\\2246z4h2\\x86" ^
+  "-BD:\\HCMUS Slide\\Nam 3\\Phat trien phan mem di dong\\Mobile_Project_Group_05\\sdk\\.cxx\\Debug\\2246z4h2\\x86" ^
   -GNinja ^
   "-DANDROID_STL=c++_shared"
