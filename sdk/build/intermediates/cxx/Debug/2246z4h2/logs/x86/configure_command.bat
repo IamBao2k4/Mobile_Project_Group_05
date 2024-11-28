1edd3f5bb42d0bdb401a29bf1f3ae2a5e4b4bfb8
ĐĂNG TRẦN XUÂN <22120047@student.hcmus.edu.vn>
2024-11-28 22:52:39 +0700

    editImage #2

--- sdk/build/intermediates/cxx/Debug/2246z4h2/logs/x86/configure_command.bat
+++ sdk/build/intermediates/cxx/Debug/2246z4h2/logs/x86/configure_command.bat
@@ -1,6 +1,6 @@
 @echo off
 "D:\\Android\\SDK\\cmake\\3.22.1\\bin\\cmake.exe" ^
-  "-HD:\\HCMUS Slide\\Nam 3\\Phat trien phan mem di dong\\Mobile_Project_Group_05\\sdk\\libcxx_helper" ^
+  "-HD:\\KHTN\\HKI\\Mobile\\Mobile_Project_Group_05\\sdk\\libcxx_helper" ^
   "-DCMAKE_SYSTEM_NAME=Android" ^
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=21" ^
@@ -11,9 +11,9 @@
   "-DCMAKE_ANDROID_NDK=D:\\Android\\SDK\\ndk\\26.1.10909125" ^
   "-DCMAKE_TOOLCHAIN_FILE=D:\\Android\\SDK\\ndk\\26.1.10909125\\build\\cmake\\android.toolchain.cmake" ^
   "-DCMAKE_MAKE_PROGRAM=D:\\Android\\SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\HCMUS Slide\\Nam 3\\Phat trien phan mem di dong\\Mobile_Project_Group_05\\sdk\\build\\intermediates\\cxx\\Debug\\2246z4h2\\obj\\x86" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\HCMUS Slide\\Nam 3\\Phat trien phan mem di dong\\Mobile_Project_Group_05\\sdk\\build\\intermediates\\cxx\\Debug\\2246z4h2\\obj\\x86" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\KHTN\\HKI\\Mobile\\Mobile_Project_Group_05\\sdk\\build\\intermediates\\cxx\\Debug\\2246z4h2\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\KHTN\\HKI\\Mobile\\Mobile_Project_Group_05\\sdk\\build\\intermediates\\cxx\\Debug\\2246z4h2\\obj\\x86" ^
   "-DCMAKE_BUILD_TYPE=Debug" ^
-  "-BD:\\HCMUS Slide\\Nam 3\\Phat trien phan mem di dong\\Mobile_Project_Group_05\\sdk\\.cxx\\Debug\\2246z4h2\\x86" ^
+  "-BD:\\KHTN\\HKI\\Mobile\\Mobile_Project_Group_05\\sdk\\.cxx\\Debug\\2246z4h2\\x86" ^
   -GNinja ^
   "-DANDROID_STL=c++_shared"
