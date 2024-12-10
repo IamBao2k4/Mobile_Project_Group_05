a86434e93d62721269544b8353e1ce86b312c662
pmgbao2004@gmail.com <pmgbao2004@gmail.com>
2024-12-10 11:53:30 +0700

    commit

--- sdk/build/intermediates/cxx/Debug/2246z4h2/logs/x86/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\ADMIN\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "E:\\University\\Semester 5\\Mobile\\Project\\Mobile_Project_Group_05\\sdk\\.cxx\\Debug\\2246z4h2\\x86" ^
-  opencv_jni_shared
